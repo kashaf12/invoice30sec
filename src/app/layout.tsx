9a1e45ef0ca375b5d9559e29d71b72d75294c6eb
kashaf12 <kashafaahmed@gmail.com>
2025-11-28 22:34:32 +0530

    feat: integrate Vercel Analytics for website usage tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from '@vercel/analytics/next';
 import "./globals.css";
 
 const inter = Inter({
@@ -34,6 +35,7 @@ export default function RootLayout({
         </div>
         <Footer />
         <Toaster />
+        <Analytics />
         <SpeedInsights />
       </body>
     </html>
